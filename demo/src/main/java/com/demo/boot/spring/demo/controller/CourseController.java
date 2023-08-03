5dbc1dda944ae4d74b5a73f84d0658f4e6c50df7
Ananya Mohanty <ananyamohanty97@gmail.com>
2023-08-03 12:23:35 +0530

    Created Course

---
+++ demo/src/main/java/com/demo/boot/spring/demo/controller/CourseController.java
@@ -0,0 +1,20 @@
+package com.demo.boot.spring.demo.controller;
+
+import com.demo.boot.spring.demo.beans.Course;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @RequestMapping("/courses")
+    public List<Course> retrieveAll(){
+        return Arrays.asList(
+                new Course(1,"AWS","in for it"),
+            new Course(2,"Java","in for it"),
+            new Course(3,"Spring","in for it")
+        );
+    }
+}
